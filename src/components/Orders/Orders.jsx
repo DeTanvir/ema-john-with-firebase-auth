888ed8241586fd3df6351aef82c6f7110813fb4d
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-04-10 02:49:22 +0600

    Order page full UI

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import Product from '../Product/Product';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css';
 
 const Orders = () => {
     const cart = useLoaderData();
 
     return (
-        <div className='shop-container'>
-            <div className="products-container">
-                <h2>These are our orders: {cart.length}</h2>
+        <div className='orders-container'>
+            <div className="review-container">
+                {
+                    cart.map(product => <ReviewItem
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
             </div>
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
