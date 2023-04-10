70f6763a2fc472efc3ef744b18aff5e5631fbb59
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-04-10 12:34:23 +0600

    delete button functionality

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,9 +3,10 @@ import './Product.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
-const Product = (props) => {
-    const { img, name, quantity, ratings, price, seller } = props.product;
-    const handleAddToCart = props.handleAddToCart;
+const Product = ({product, handleAddToCart}) => {
+    const { img, name, quantity, ratings, price, seller } = product;
+    
+    // const handleAddToCart = handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -15,7 +16,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Ratings: {ratings} Stars</p>
             </div>
-            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart <FontAwesomeIcon icon={faCartPlus} /></button>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>Add to Cart <FontAwesomeIcon icon={faCartPlus} /></button>
         </div>
     );
 };
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -3,7 +3,8 @@ import './ReviewItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
+    // console.log(handleRemoveFromCart);
     const {id, name, price, img, quantity} = product;
     return (
         <div className='review-item'>
@@ -13,7 +14,9 @@ const ReviewItem = ({product}) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
-            <button className="btn-delete"><FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /></button>
+            <button onClick={() => handleRemoveFromCart(id)} className="btn-delete">
+                <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
